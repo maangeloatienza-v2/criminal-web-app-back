f757d2a2078b8422a8d6ac9b438db4af1e646402
maangeloatienza <maangeloatienza@gmail.com>
2019-02-26 20:21:56 +0800

    add db

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ DEVELOPMENT = false;
 
 PORT = 8084;
 
-MYSQL_HOST = DEVELOPMENT ?process.env.MYSQL_HOST : '85.10.205.173:3306' //'localhost';
+MYSQL_HOST = DEVELOPMENT ?process.env.MYSQL_HOST : 'db4free.net'//'85.10.205.173:3306' //'localhost';
 MYSQL_USER = DEVELOPMENT ?process.env.MYSQL_USER : 'agri_thesis' //'root';
 MYSQL_PASSWORD = DEVELOPMENT ?process.env.MYSQL_PASSWORD : 'secretPassword123'// '12345';
 MYSQL_DATABASE = DEVELOPMENT ?process.env.MYSQL_DATABASE : 'agri_thesis'// 'criminal_watch_db';
