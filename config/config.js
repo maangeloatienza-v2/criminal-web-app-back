22410026844db610fa6d1096d0ab5eae096a1121
maangeloatienza <maangeloatienza@gmail.com>
2019-03-20 13:23:13 +0800

    Fix db config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-DEVELOPMENT = false;
+DEVELOPMENT = true;
 
 PORT = 8084;
 
-MYSQL_HOST = DEVELOPMENT ?process.env.MYSQL_HOST : 'db4free.net'//'85.10.205.173:3306' //'localhost';
-MYSQL_USER = DEVELOPMENT ?process.env.MYSQL_USER : 'agri_thesis' //'root';
-MYSQL_PASSWORD = DEVELOPMENT ?process.env.MYSQL_PASSWORD : 'secretPassword123'// '12345';
-MYSQL_DATABASE = DEVELOPMENT ?process.env.MYSQL_DATABASE : 'agri_thesis'// 'criminal_watch_db';
+MYSQL_HOST = DEVELOPMENT ?'ip-172-31-24-97'/*process.env.MYSQL_HOST*/ : 'db4free.net'//'85.10.205.173:3306' //'localhost';
+MYSQL_USER = DEVELOPMENT ?'root'/*process.env.MYSQL_USER*/ : 'agri_thesis' //'root';
+MYSQL_PASSWORD = DEVELOPMENT ?'secretPassword123'/*process.env.MYSQL_PASSWORD*/ : 'secretPassword123'// '12345';
+MYSQL_DATABASE = 'agri_thesis';// 'criminal_watch_db';
 
 
 JWT_TOKEN      = 'secret_key';
--- config/db_config.js
+++ config/db_config.js
@@ -4,7 +4,7 @@ require('./config').config;
 const MASTER_DB = {
   host : MYSQL_HOST || 'localhost',
   user : MYSQL_USER || 'root',
-  password : MYSQL_PASSWORD || '12345',
+  password : MYSQL_PASSWORD || 'secretPassword123',
   database : MYSQL_DATABASE || 'agri_thesis',
   retryable_errors: ['ER_LOCK_DEADLOCK', 'PROTOCOL_SEQUENCE_TIMEOUT']
 };
