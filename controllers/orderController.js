cb856b6bf2c0a1ca3631a530703213f621bde887
maangeloatienza <maangeloatienza@gmail.com>
2019-03-26 12:08:00 +0800

    Comment order v1

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,303 +1,303 @@
-'use strict';
+// 'use strict';
 
-const mysql             = require('anytv-node-mysql');
-const util              = require('./../helpers/util');
-const uuidv4            = require('uuid/v4');
-const jwt               = require('jsonwebtoken');
-const nodemailer 		= require('nodemailer');
-const err_response      = require('./../libraries/response').err_response;
-const tx_code      		= require('./../libraries/code_generator').randomAlphanumeric;
-                          require('./../config/err_config');
-                          require('./../config/config');
+// const mysql             = require('anytv-node-mysql');
+// const util              = require('./../helpers/util');
+// const uuidv4            = require('uuid/v4');
+// const jwt               = require('jsonwebtoken');
+// const nodemailer 		= require('nodemailer');
+// const err_response      = require('./../libraries/response').err_response;
+// const tx_code      		= require('./../libraries/code_generator').randomAlphanumeric;
+//                           require('./../config/err_config');
+//                           require('./../config/config');
 
-const products = [
-	{
-		product_name : '',
-		item_price : 1.0,
-		quantity : 1
-	}
-]
+// const products = [
+// 	{
+// 		product_name : '',
+// 		item_price : 1.0,
+// 		quantity : 1
+// 	}
+// ]
 
-/**
- * @api {post} v1/add-orders               	Create order 
- * @apiName Create Order
- * @apiGroup Orders
- * 
- * 
- * @apiParam {Array}       products[]      			Array of products 
- * @apiParam {String}      products.product_name  Product name
- * @apiParam {Float}       products.item_price    Product price
- * @apiParam {Int}         products.quantity      Product quantity
+// /**
+//  * @api {post} v1/add-orders               	Create order 
+//  * @apiName Create Order
+//  * @apiGroup Orders
+//  * 
+//  * 
+//  * @apiParam {Array}       products[]      			Array of products 
+//  * @apiParam {String}      products.product_name  Product name
+//  * @apiParam {Float}       products.item_price    Product price
+//  * @apiParam {Int}         products.quantity      Product quantity
 
- */
-
-
-async function check_tx_code(tx_code){
-
-	let query = `SELECT * FROM orders WHERE code = '${tx_code}'`;
-
-	return await (mysql.build(query).promise());
-
-}
+//  */
 
 
-async function insert_order(req,items){
+// async function check_tx_code(tx_code){
 
-	let query = `INSERT INTO orders SET ?`;
-	let err_flag = 0;
-	let data_holder = {};
-	let txCode,code,error;
+// 	let query = `SELECT * FROM orders WHERE code = '${tx_code}'`;
+
+// 	return await (mysql.build(query).promise());
+
+// }
+
+
+// async function insert_order(req,items){
+
+// 	let query = `INSERT INTO orders SET ?`;
+// 	let err_flag = 0;
+// 	let data_holder = {};
+// 	let txCode,code,error;
 	
 
-	txCode = tx_code();
+// 	txCode = tx_code();
     
-    [error,code] = await to(check_tx_code(txCode.code));
+//     [error,code] = await to(check_tx_code(txCode.code));
 
-    if(code.length){
-    	return err_response(res,EXISTING,EXISTING,500);
-    }
+//     if(code.length){
+//     	return err_response(res,EXISTING,EXISTING,500);
+//     }
 
-	await items.map(item=>{
+// 	await items.map(item=>{
 
-		item.code = txCode;
-		item.id = uuidv4();
-		item.created = new Date();
-  		item.total_item = ( item.quantity * item.item_price);
-  		item.order_by = req.user.id; 
+// 		item.code = txCode;
+// 		item.id = uuidv4();
+// 		item.created = new Date();
+//   		item.total_item = ( item.quantity * item.item_price);
+//   		item.order_by = req.user.id; 
 
-		(mysql.build(query,item)
-		 	.promise()
-		 	.then(res =>{
-		 	})
-		 	.catch(err=>{
-		 			err_flag = 1
-		 		}
+// 		(mysql.build(query,item)
+// 		 	.promise()
+// 		 	.then(res =>{
+// 		 	})
+// 		 	.catch(err=>{
+// 		 			err_flag = 1
+// 		 		}
 
-		 	));
-	});
+// 		 	));
+// 	});
 
-	if(err_flag == 1 ){
-		return false;
-	}
+// 	if(err_flag == 1 ){
+// 		return false;
+// 	}
 
-	return true;
+// 	return true;
 
 	
 
-}
+// }
 
-const addOrder = (req,res,next)=>{
-	res.setHeader('Content-Type', 'application/json');
-	const data = util._get
-    .form_data(products)
-    .from(req.body);
+// const addOrder = (req,res,next)=>{
+// 	res.setHeader('Content-Type', 'application/json');
+// 	const data = util._get
+//     .form_data(products)
+//     .from(req.body);
 
-    let error,code,order;
+//     let error,code,order;
 
-    let user = req.user;
+//     let user = req.user;
 	
-	async function start(){
+// 	async function start(){
 
-		if(!data){
-			return err_response(res,INC_DATA,INC_DATA,404);
-		}
+// 		if(!data){
+// 			return err_response(res,INC_DATA,INC_DATA,404);
+// 		}
 
-        [error,order] = await to(insert_order(req,data));
+//         [error,order] = await to(insert_order(req,data));
 
 
-        if(!order){
+//         if(!order){
 
-            return err_response(res,NO_RECORD_CREATED,NO_RECORD_CREATED,404);
+//             return err_response(res,NO_RECORD_CREATED,NO_RECORD_CREATED,404);
 
-        }
+//         }
 
         
 
-        return res.json({
-        	data : data,
-        	message : 'Order created successfully',
-        	context : 'Data created successfully'
-        })
-        .status(200)
-        .send();
+//         return res.json({
+//         	data : data,
+//         	message : 'Order created successfully',
+//         	context : 'Data created successfully'
+//         })
+//         .status(200)
+//         .send();
 
-	}
+// 	}
 
-	start();
+// 	start();
 
-}
-
-
-/**
- * @api {get} v1/orders               	Fetch orders 
- * @apiName Get Orders
- * @apiGroup Orders
- * 
- * 
- * @apiParam {String}      [search]      			Search order by first_name,last_name,username.code 
- * @apiParam {String}      [product_name]          Filter matching Product name
- * @apiParam {String}      [username]			    Filter by username
- * @apiParam {String}      [first_name]			Filter by first_name
- * @apiParam {String}      [last_name]			    Filter by last_name
- */
+// }
 
 
+// *
+//  * @api {get} v1/orders               	Fetch orders 
+//  * @apiName Get Orders
+//  * @apiGroup Orders
+//  * 
+//  * 
+//  * @apiParam {String}      [search]      			Search order by first_name,last_name,username.code 
+//  * @apiParam {String}      [product_name]          	Filter matching Product name
+//  * @apiParam {String}      [username]			    Filter by username
+//  * @apiParam {String}      [first_name]				Filter by first_name
+//  * @apiParam {String}      [last_name]			    Filter by last_name
+ 
 
 
-const getAll = (req,res,next)=>{
-	res.setHeader('Content-Type', 'application/json');
-	const {
-		search,
-		username,
-		first_name,
-		last_name,
-		product_name
-	} = req.query;
-
-	let where = ` WHERE o.deleted is null `;
-
-	if(search){
-		where += 
-		` 	AND o.code LIKE '%${search}%' \
-		  	OR o.product_name LIKE '%${search}%' \
-		`;
-	}
 
 
-	if(product_name){
-		where +=
-		`
-			AND o.product_name = '${product_name}'\
-		`;
-	}
+// const getAll = (req,res,next)=>{
+// 	res.setHeader('Content-Type', 'application/json');
+// 	const {
+// 		search,
+// 		username,
+// 		first_name,
+// 		last_name,
+// 		product_name
+// 	} = req.query;
 
-	if(username){
-		where +=
-		`
-			AND user.username = '${username}'\
-		`;
-	}
+// 	let where = ` WHERE o.deleted is null `;
 
-	if(first_name){
-		where +=
-		`
-			AND user.first_name = '${first_name}'\
-		`;
-	}
-
-	if(last_name){
-		where +=
-		`
-			AND user.last_name = '${last_name}'\
-		`;
-	}
+// 	if(search){
+// 		where += 
+// 		` 	AND o.code LIKE '%${search}%' \
+// 		  	OR o.product_name LIKE '%${search}%' \
+// 		`;
+// 	}
 
 
-	let query = 
-	`	SELECT \ 
-		o.*,
-		user.username, \
-		user.first_name, \
-		user.last_name \
-		FROM orders o \
-		LEFT JOIN users user \
-		ON user.id = o.order_by \
-		${where}
-	`;
+// 	if(product_name){
+// 		where +=
+// 		`
+// 			AND o.product_name = '${product_name}'\
+// 		`;
+// 	}
 
-	function start(){
+// 	if(username){
+// 		where +=
+// 		`
+// 			AND user.username = '${username}'\
+// 		`;
+// 	}
 
-		mysql.use('master')
-			.query(query,send_response)
-			.end()
+// 	if(first_name){
+// 		where +=
+// 		`
+// 			AND user.first_name = '${first_name}'\
+// 		`;
+// 	}
 
-	}
+// 	if(last_name){
+// 		where +=
+// 		`
+// 			AND user.last_name = '${last_name}'\
+// 		`;
+// 	}
 
-	function send_response(err,result,args,last_query){
-		if(err){
-			return err_response(res,BAD_REQ,BAD_REQ,500)
-		}
+
+// 	let query = 
+// 	`	SELECT \ 
+// 		o.*,
+// 		user.username, \
+// 		user.first_name, \
+// 		user.last_name \
+// 		FROM orders o \
+// 		LEFT JOIN users user \
+// 		ON user.id = o.order_by \
+// 		${where}
+// 	`;
+
+// 	function start(){
+
+// 		mysql.use('master')
+// 			.query(query,send_response)
+// 			.end()
+
+// 	}
+
+// 	function send_response(err,result,args,last_query){
+// 		if(err){
+// 			return err_response(res,BAD_REQ,BAD_REQ,500)
+// 		}
 		
-		if(!result.length){
-			return err_response(res,ZERO_RES,ZERO_RES,400);
-		}
+// 		if(!result.length){
+// 			return err_response(res,ZERO_RES,ZERO_RES,400);
+// 		}
 
-		return res.json({
-			data : result,
-			message : 'Successfully fetched orders',
-			context : 'Data fetched successfully'
-		})
-		.status(200)
-		.send();
+// 		return res.json({
+// 			data : result,
+// 			message : 'Successfully fetched orders',
+// 			context : 'Data fetched successfully'
+// 		})
+// 		.status(200)
+// 		.send();
 
-	}
+// 	}
 
-	start();
-}
-
-
-/**
- * @api {get} v1/orders/:id               	Fetch specific order 
- * @apiName Get Order
- * @apiGroup Orders
- * 
- */
+// 	start();
+// }
 
 
-const getOne = (req,res,next)=>{
-	res.setHeader('Content-Type', 'application/json');
-	const id = req.params.id;
+// /**
+//  * @api {get} v1/orders/:id               	Fetch specific order 
+//  * @apiName Get Order
+//  * @apiGroup Orders
+//  * 
+//  */
 
-	let where = ` WHERE o.deleted is null AND o.id = '${id}'`;
+
+// const getOne = (req,res,next)=>{
+// 	res.setHeader('Content-Type', 'application/json');
+// 	const id = req.params.id;
+
+// 	let where = ` WHERE o.deleted is null AND o.id = '${id}'`;
 
 
-	let query = 
-	`	SELECT \ 
-		o.*,
-		user.username, \
-		user.first_name, \
-		user.last_name \
-		FROM orders o \
-		LEFT JOIN users user \
-		ON user.id = o.order_by \
-		${where}
-	`;
+// 	let query = 
+// 	`	SELECT \ 
+// 		o.*,
+// 		user.username, \
+// 		user.first_name, \
+// 		user.last_name \
+// 		FROM orders o \
+// 		LEFT JOIN users user \
+// 		ON user.id = o.order_by \
+// 		${where}
+// 	`;
 
-	function start(){
+// 	function start(){
 
-		mysql.use('master')
-			.query(query,send_response)
-			.end()
+// 		mysql.use('master')
+// 			.query(query,send_response)
+// 			.end()
 
-	}
+// 	}
 
-	function send_response(err,result,args,last_query){
+// 	function send_response(err,result,args,last_query){
 
-		if(err){
-			return err_response(res,BAD_REQ,BAD_REQ,500)
-		}
+// 		if(err){
+// 			return err_response(res,BAD_REQ,BAD_REQ,500)
+// 		}
 		
-		if(!result.length){
-			return err_response(res,ZERO_RES,ZERO_RES,400);
-		}
+// 		if(!result.length){
+// 			return err_response(res,ZERO_RES,ZERO_RES,400);
+// 		}
 
-		return res.json({
-			data : result[0],
-			message : 'Successfully fetched orders',
-			context : 'Data fetched successfully'
-		})
-		.status(200)
-		.send();
+// 		return res.json({
+// 			data : result[0],
+// 			message : 'Successfully fetched orders',
+// 			context : 'Data fetched successfully'
+// 		})
+// 		.status(200)
+// 		.send();
 
-	}
+// 	}
 
-	start();
-}
+// 	start();
+// }
 
 
 
-module.exports = {
-	addOrder,
-	getAll,
-	getOne
-}
+// module.exports = {
+// 	addOrder,
+// 	getAll,
+// 	getOne
+// }
